b9964067e0301c05880cb4fea0db23cb3f4463ca
Gildraen <gaetan.coleno@free.fr>
2017-06-27 10:13:46 +0200

    Divers modif

--- app/src/main/java/fr/cesi/base/ui/orthographe/CorrectionMotFragment.java
+++ app/src/main/java/fr/cesi/base/ui/orthographe/CorrectionMotFragment.java
@@ -50,10 +50,6 @@ public class CorrectionMotFragment extends Fragment implements IPopableFragment 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
