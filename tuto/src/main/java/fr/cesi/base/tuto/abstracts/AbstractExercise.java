2356e03af6ef61a01da10667862da8d37315dbe5
Gildraen <gaetan.coleno@free.fr>
2017-06-29 11:55:31 +0200

    tuto
    tuto

--- tuto/src/main/java/fr/cesi/base/tuto/abstracts/AbstractExercise.java
+++ tuto/src/main/java/fr/cesi/base/tuto/abstracts/AbstractExercise.java
@@ -4,7 +4,7 @@ package fr.cesi.base.tuto.abstracts;
  * Created by kevinleperf on 22/06/2017.
  */
 
-public abstract class AbstractExercise {
+public abstract class  AbstractExercise {
 
     public abstract boolean startExercise();
 }
